f4a50d7fbae0aaaf206f156fb943cec3fb406eab
rahuldev9 <webdeveloperaim@gmail.com>
2025-02-20 23:30:29 +0530

    Update Nav.js

--- client/src/Components/Nav.js
+++ client/src/Components/Nav.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import Changename from "./Changename";
 
 function Nav() {
   const [auth, setAuth] = useState(null);
+  const [name,setname]=useState(false);
+
   const navigate = useNavigate();
   const maleAvatar = "/male.png"; // Update with the actual path
   const femaleAvatar = "/female.png"; // Update with the actual path
@@ -17,6 +20,13 @@ function Nav() {
     navigate('/')
     window.location.reload();
   }
+  const changename=()=>{
+    setname(true)
+  }
+  const onclose = () => {
+    setname(false)
+    
+  };
 
   return (
     <div>
@@ -43,15 +53,18 @@ function Nav() {
                 height: 50,
                 borderRadius: "50%",
                 margin: "5px",
+                cursor:'pointer',
               }}
+              onClick={changename}
             />
-            <p style={{ margin: "2px", textAlign: "center" }}>@{auth.name}</p>
+            <p style={{ margin: "2px", textAlign: "center" }} >@{auth.name}</p>
           </div>
           <button onClick={leaveroom} className="Btn"><i class="fa-solid fa-right-from-bracket"></i></button>
         </div>
       ) : (
         <div style={{ display: "flex", flexDirection: "row" }}></div>
       )}
+      {name && <Changename close={onclose}/>}
     </div>
   );
 }
