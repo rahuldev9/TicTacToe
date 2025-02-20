e388871b094a4a3e00e8741796c0d421f39b9f6c
rahuldev9 <webdeveloperaim@gmail.com>
2025-02-20 22:43:55 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,34 @@
 import React from "react";
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 import Start from "./Components/Start";
 import Home from "./Components/Home";
 
+function ProtectedRoute({ children }) {
+  const hasData = localStorage.getItem("user"); // Change key as needed
+  return hasData ? children : <Navigate to="/" />;
+}
+
 function App() {
-  
   return (
-    <>
-    <div id='background'>
+    <div id="background">
       <BrowserRouter>
-      <Routes>
-      <Route path="/" element={<Home/>}></Route>
-        <Route path="/start" element={<Start/>}></Route>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/start"
+            element={
+              <ProtectedRoute>
+                <Start />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
       </BrowserRouter>
     </div>
-    </>
   );
 }
 
 export default App;
+
